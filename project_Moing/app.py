dc957a36ac747b2380161622b9a3ccc52b71c7a6
Juri <haribo__@naver.com>
2020-08-12 22:34:54 +0900

    메인, 로그인 화면 구현

---
+++ project_Moing/app.py
@@ -0,0 +1,24 @@
+from pymongo import MongoClient
+
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)
+db = client.dbmoing
+
+# HTML 화면 보여주기
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# 로그인 화면 보여주기
+@app.route('/login')
+def logintoss():
+    return render_template('loginpage.html')
+
+
+
+
+if __name__ == '__main__':
+    app.run('127.0.0.1', port=5000, debug=True)
